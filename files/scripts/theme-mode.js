fe38dc5ac0c2d6f855d3d07b8ac43961469213f4
Qualialog <139740907+qualialog@users.noreply.github.com>
2023-07-30 06:12:29 +0400

    add rectangle

--- files/scripts/theme-mode.js
+++ files/scripts/theme-mode.js
@@ -1,6 +1,8 @@
 // Shortcut: control ;
 // Name: theme mode
 
-import "@johnlindquist/kit"
+import "@johnlindquist/kit";
 
-await exec(`osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to not dark mode'`)
+await exec(
+  `osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to not dark mode'`
+);
